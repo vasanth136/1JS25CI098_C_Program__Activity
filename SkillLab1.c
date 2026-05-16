924d61919b440b9f95311331f3a25e32fa8e0600
vasanth136 <vasanthkallutti@gmail.com>
2026-05-16 12:10:21 +0530

    SkillLab1.c

---
+++ SkillLab1.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+
+int main() {
+
+  printf("Name :- VASANTH K\n");
+  printf("USN:- 1JS25CI098\n");
+    int a, b, sum;
+
+    scanf("%d %d", &a, &b);
+
+    sum = a + b;
+
+    printf("%d", sum);
+
+    return 0;
+}
